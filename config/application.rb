9cd0a4a2d4e965c47a701522ace4b9d1e5aa14d0
Peter Yates <peter.yates@graphia.co.uk>
2024-07-19 15:34:01 +0100

    Add Rubocop config and fix any failures
    We're going to ignore any files that are frequently automatically
    generated and I've disabled some rules right off the bat:
    
    Style/StringLiterals              => Pointlessly whiny
    Style/EmptyMethod                 => Pointlessly whiny
    Style/TrailingCommaInArguments    => Ugly
    Style/TrailingCommaInHashLiteral  => Ugly
    Style/TrailingCommaInArrayLiteral => Ugly
    Style/EmptyCaseCondition          => Empty case is way more readable
                                         than if/elsif/else
    Style/RedundantParentheses        => A parentheses that helps
                                         readability isn't redundant
    Style/AsciiComments               => Sometimes I want an emoji in
                                         my comment damnit
    Style/SignalException             => There's a good reason why both
                                         `fail` and `raise` are included
                                         in the language, let's follow
                                         Jim Weirich's advice[0]
    
    [0] https://avdi.codes/jim-weirich-on-exceptions/

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ Bundler.require(*Rails.groups)
 module Ecf2
   class Application < Rails::Application
     config.load_defaults 7.1
-    config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_lib(ignore: %w[assets tasks])
     config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets')
     config.exceptions_app = routes
   end
