4152fe56b91d848e80b276fe4159a4544cf3a116
Tony Headford <tony@binarycircus.com>
2024-07-19 13:42:06 +0100

    Add and configure Rails
    This Rails app was generated using DfE's Rails template. We have
    customised it in the following ways:
    
    * removed many of the Rails-genreated comments in config files
    * added some common inflections like API and ECT
    * added FactoryBot
    * use NPM instead of Yarn (this means calling esbuild and sass directly)
    * bumped to Ruby 3.3.4
    * bumped to Node 22.5
    
    We have tested:
    
    * the Docker image builds successfully
    * bin/setup and bin/dev work on both Linux/x86-64 and darwin/Arm
    
    Co-authored-by: peteryates <peter.yates@graphia.co.uk>

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source "https://rubygems.org"
+
+ruby "3.3.4"
+
+gem "rails", "~> 7.1.3", ">= 7.1.3.4"
+
+gem "bootsnap", require: false
+gem "cssbundling-rails"
+gem "jsbundling-rails"
+gem "pg", "~> 1.1"
+gem "propshaft"
+gem "puma", ">= 5.0"
+gem "tzinfo-data", platforms: %i[windows jruby]
+
+gem "govuk-components"
+gem "govuk_design_system_formbuilder"
+
+group :development, :test do
+  gem "debug", platforms: %i[mri windows]
+  gem "factory_bot_rails"
+  gem "faker"
+  gem "rspec"
+  gem "rspec-rails"
+  gem "rubocop-govuk"
+end
+
+group :test do
+  gem "capybara"
+end
---
+++ config/application.rb
@@ -0,0 +1,21 @@
+require_relative "boot"
+
+require "rails"
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+
+Bundler.require(*Rails.groups)
+
+module Ecf2
+  class Application < Rails::Application
+    config.load_defaults 7.1
+    config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.paths << Rails.root.join('node_modules/govuk-frontend/dist/govuk/assets')
+    config.exceptions_app = routes
+  end
+end
