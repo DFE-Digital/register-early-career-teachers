c8e074745d3508d1745cc830684b5a3d181570e6
Steve Laing <steve.laing@gmail.com>
2024-08-06 17:40:50 +0100

    Fix example mailer spec expectation
    This was failing because of a notify gem default from: address

--- app/mailers/example_mailer.rb
+++ app/mailers/example_mailer.rb
@@ -1,11 +1,12 @@
 class ExampleMailer < ApplicationMailer
   def hello_world
+    from = params.fetch(:from)
     to = params.fetch(:to)
     subject = params.fetch(:subject)
     @salutation = params.fetch(:salutation)
 
     @recipient = Data.define(:full_name).new(full_name: "Herbert M Anchovy")
 
-    view_mail(NOTIFY_TEMPLATE_ID, to:, subject:)
+    view_mail(NOTIFY_TEMPLATE_ID, from:, to:, subject:)
   end
 end
