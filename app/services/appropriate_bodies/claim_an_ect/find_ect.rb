6630fd66cf2f192e43b67beea39f4d593b781fa5
Jack S <jack.scotti@education.gov.uk>
2024-09-19 16:26:22 +0100

    Retrieve and display teacher data

--- app/services/appropriate_bodies/claim_an_ect/find_ect.rb
+++ app/services/appropriate_bodies/claim_an_ect/find_ect.rb
@@ -9,8 +9,6 @@ module AppropriateBodies
       end
 
       def import_from_trs
-        # TODO: call the TRS API and search for a teacher with matching
-        #       TRN and date of birth
         # TODO: what do we do if we already have a matching Teacher in
         #       our database
         #       a) as a fully registered teacher?
@@ -31,30 +29,10 @@ module AppropriateBodies
     private
 
       def find_matching_record_in_trs
-        # TODO: build query, just fake for now, return false if we find nothing
-        #       response is in this format:
-        # TODO: the API client should return an object
-        # {
-        #   "trn": "3001924",
-        #   "firstName": "First name",
-        #   "middleName": "Middle name",
-        #   "lastName": "Last name",
-        #   "dateOfBirth": "1990-01-03",
-        #   "nationalInsuranceNumber": null,
-        #   "emailAddress": null,
-        #   "qts": {
-        #     "awarded": "2022-07-15",
-        #     "certificateUrl": "/v3/certificates/qts",
-        #     "statusDescription": "Qualified"
-        #   },
-        #   "eyts": null
-        # }
-        # TODO: do we need to do anything with middleName?
-        # TODO: we need to store all the details we'll show on the check screen
-        #       in the PendingInductionSubmission so we can display it on the
-        #       'check' stage
+        client = TRS::APIClient.new
+        teacher = client.find_teacher(trn: pending_induction_submission.trn, date_of_birth: pending_induction_submission.date_of_birth)
 
-        { trn: "3456345", first_name: "Kirk", last_name: "Van Houten", date_of_birth: Date.new(1970, 1, 1) }
+        teacher&.present
       end
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_15_123828) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_18_160307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -158,6 +158,28 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_15_123828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "confirmed_at"
+    t.string "middle_name"
+    t.string "national_insurance_number"
+    t.string "email_address"
+    t.boolean "eyts_awarded"
+    t.string "eyts_certificate_url"
+    t.string "eyts_status_description"
+    t.jsonb "alerts"
+    t.date "induction_start_date"
+    t.date "induction_end_date"
+    t.string "induction_status_description"
+    t.string "induction_certificate_url"
+    t.boolean "pending_name_change"
+    t.boolean "pending_date_of_birth_change"
+    t.boolean "qts_awarded"
+    t.string "qts_certificate_url"
+    t.string "qts_status_description"
+    t.jsonb "initial_teacher_training"
+    t.jsonb "npq_qualifications"
+    t.jsonb "mandatory_qualifications"
+    t.jsonb "higher_education_qualifications"
+    t.jsonb "previous_names"
+    t.boolean "allow_id_sign_in_with_prohibitions"
     t.index ["appropriate_body_id"], name: "index_pending_induction_submissions_on_appropriate_body_id"
   end
 
