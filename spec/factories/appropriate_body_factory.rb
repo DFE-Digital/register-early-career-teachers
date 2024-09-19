ad36786b913a1fd929218431c6a179ceb39af0aa
Steve Laing <steve.laing@gmail.com>
2024-09-19 16:15:42 +0100

    Add admin roles form

--- spec/factories/appropriate_body_factory.rb
+++ spec/factories/appropriate_body_factory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory(:appropriate_body) do
-    name { "#{Faker::Lorem.word.capitalize} AB" }
+    name { "#{Faker::Lorem.unique.word.capitalize} AB" }
     sequence(:local_authority_code, 100)
     sequence(:establishment_number, 1000)
   end
