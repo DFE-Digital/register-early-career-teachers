8f4dbba908f8f36f215687467cfca2ddb10c6702
Tony Headford <tony@binarycircus.com>
2024-09-24 15:21:27 +0100

    Database config changes

--- app/models/migration/base.rb
+++ app/models/migration/base.rb
@@ -2,7 +2,7 @@ module Migration
   class Base < ApplicationRecord
     self.abstract_class = true
 
-    connects_to database: { reading: :legacy_ecf, writing: :legacy_ecf } unless Rails.env.test?
+    connects_to database: { reading: :legacy, writing: :legacy }
 
     def readonly?
       !Rails.env.test? # allow factories in test
