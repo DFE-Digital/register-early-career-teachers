577b4190c5d27fa115397a3f292a1a43f0f1a9d6
Tony Headford <tony@binarycircus.com>
2024-09-24 21:14:43 +0100

    Dont connect legacy in test for now

--- app/models/migration/base.rb
+++ app/models/migration/base.rb
@@ -2,7 +2,7 @@ module Migration
   class Base < ApplicationRecord
     self.abstract_class = true
 
-    connects_to database: { reading: :legacy, writing: :legacy }
+    connects_to database: { reading: :legacy, writing: :legacy } unless Rails.env.test?
 
     def readonly?
       !Rails.env.test? # allow factories in test
