60cb6619f12407594179682da7130aa66674a427
Jack S <jack.scotti@education.gov.uk>
2024-10-14 16:17:06 +0100

    Refactor test and extract fake session manager context

--- spec/requests/admin/appropriate_bodies/index_spec.rb
+++ spec/requests/admin/appropriate_bodies/index_spec.rb
@@ -5,46 +5,36 @@ RSpec.describe "Admin", type: :request do
       expect(response).to redirect_to(sign_in_path)
     end
 
-    context "with an authenticated user" do
-      let(:session_manager) do
-        instance_double(Sessions::SessionManager, provider: "developer", expires_at: 2.hours.from_now)
-      end
-      let(:user) { FactoryBot.create(:user) }
-
-      before do
-        allow(Sessions::SessionManager).to receive(:new).and_return(session_manager)
-        allow(session_manager).to receive(:load_from_session).and_return(user)
-      end
+    context "with an authenticated non-DfE user" do
+      include_context 'fake session manager for non-DfE user'
 
       it "requires authorisation" do
         get "/admin/appropriate-bodies"
 
         expect(response.status).to eq(401)
       end
+    end
 
-      context "when DfE user" do
-        before do
-          FactoryBot.create(:dfe_role, user:)
-        end
+    context "with an authenticated DfE user" do
+      include_context 'fake session manager for DfE user'
 
-        let!(:appropriate_body1) { FactoryBot.create(:appropriate_body, name: "Captain Scrummy") }
-        let!(:appropriate_body2) { FactoryBot.create(:appropriate_body, name: "Captain Hook") }
+      let!(:appropriate_body1) { FactoryBot.create(:appropriate_body, name: "Captain Scrummy") }
+      let!(:appropriate_body2) { FactoryBot.create(:appropriate_body, name: "Captain Hook") }
 
-        it "display appropriate bodies" do
-          get "/admin/appropriate-bodies"
+      it "display appropriate bodies" do
+        get "/admin/appropriate-bodies"
 
+        expect(response.status).to eq(200)
+        expect(response.body).to include("Captain Scrummy", "Captain Hook")
+      end
+
+      context "when searching for appropriate bodies" do
+        it "displays search results" do
+          get "/admin/appropriate-bodies?q=Hook"
           expect(response.status).to eq(200)
-          expect(response.body).to include("Captain Scrummy", "Captain Hook")
-        end
 
-        context "when searching for appropriate bodies" do
-          it "displays search results" do
-            get "/admin/appropriate-bodies?q=Hook"
-            expect(response.status).to eq(200)
-
-            expect(response.body).to include("Captain Hook")
-            expect(response.body).not_to include("Captain Scrummy")
-          end
+          expect(response.body).to include("Captain Hook")
+          expect(response.body).not_to include("Captain Scrummy")
         end
       end
     end
