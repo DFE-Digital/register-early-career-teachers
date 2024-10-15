0aa3e2b40c0df57840e8e1b1220d028b20a9d395
Peter Yates <peter.yates@graphia.co.uk>
2024-10-15 17:19:16 +0100

    Add pass and fail actions to TRS API client
    These make use of the new endpoint `/v3/persons/{trn}/induction`
    which allows us to set a status. It's hardcoded to respond with 204
    for now and available in preprod[0].
    
    The valid statues are currently but liable to change:
    
      0 = "Exempt"
      1 = "Fail"
      2 = "FailedinWales"
      3 = "InductionExtended"
      4 = "InProgress"
      5 = "NotYetCompleted"
      6 = "Pass"
      7 = "PassedinWales"
      8 = "RequiredtoComplete"
    
    [0] https://preprod.teacher-qualifications-api.education.gov.uk/swagger/index.html

--- lib/TRS/api_client.rb
+++ lib/TRS/api_client.rb
@@ -4,13 +4,14 @@ module TRS
       @connection = Faraday.new(url: ENV['TRS_BASE_URL']) do |faraday|
         faraday.headers['Authorization'] = "Bearer #{ENV['TRS_API_KEY']}"
         faraday.headers['Accept'] = 'application/json'
-        faraday.headers['X-Api-Version'] = '20240814'
+        faraday.headers['X-Api-Version'] = 'Next'
+        faraday.headers['Content-Type'] = 'application/json'
         faraday.adapter Faraday.default_adapter
       end
     end
 
     def find_teacher(trn:, date_of_birth:)
-      response = @connection.get("/v3/persons/#{trn}", dateOfBirth: date_of_birth)
+      response = @connection.get(persons_path(trn), dateOfBirth: date_of_birth)
 
       if response.success?
         TRS::Teacher.new(JSON.parse(response.body))
@@ -24,14 +25,45 @@ module TRS
       end
     end
 
-    # FIXME: Update this method after the API is implemented
     def begin_induction!(trn:, start_date:)
-      Rails.logger.info("TRS API: begin_induction(#{trn}, #{start_date})")
+      update_induction_status(trn:, status: 'InProgress', start_date:)
     end
 
-    # FIXME: Update this method after the API is implemented
-    def complete_induction!(trn:, completion_date:, status:)
-      Rails.logger.info("TRS API: complete_induction(#{trn}, #{completion_date}, #{status})")
+    def pass_induction!(trn:, completion_date:)
+      update_induction_status(trn:, status: 'Pass', completion_date:)
+    end
+
+    def fail_induction!(trn:, completion_date:)
+      update_induction_status(trn:, status: 'Fail', completion_date:)
+    end
+
+  private
+
+    def update_induction_status(trn:, status:, start_date: nil, completion_date: nil)
+      payload = { 'inductionStatus' => status,
+                  'startDate' => start_date,
+                  'completionDate' => completion_date }.compact.to_json
+
+      response = @connection.put(persons_path(trn, suffix: 'induction'), payload)
+
+      Rails.logger.debug("calling TRS API: #{response}")
+
+      if response.success?
+        Rails.logger.debug("OK")
+
+        # FIXME: is there anything that comes back in legit responses that
+        #        we want to keep hold of?
+        true
+      else
+        Rails.logger.warn("Error: #{response.status}")
+        Rails.logger.warn("Response: #{response.body}")
+
+        false
+      end
+    end
+
+    def persons_path(trn, suffix: nil)
+      ['v3', 'persons', trn, suffix].compact.join('/')
     end
   end
 end
--- spec/lib/TRS/api_client_spec.rb
+++ spec/lib/TRS/api_client_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe TRS::APIClient do
   let(:client) { described_class.new }
   let(:trn) { '1234567' }
   let(:date_of_birth) { '1990-01-01' }
+  let(:connection) { client.instance_variable_get(:@connection) }
 
   describe '#find_teacher' do
     context 'when the API request is successful' do
@@ -9,7 +10,7 @@ RSpec.describe TRS::APIClient do
       let(:response) { instance_double(Faraday::Response, success?: true, body: response_body) }
 
       before do
-        allow(client.instance_variable_get(:@connection)).to receive(:get).and_return(response)
+        allow(connection).to receive(:get).and_return(response)
       end
 
       it 'returns a TRS::Teacher object' do
@@ -39,7 +40,7 @@ RSpec.describe TRS::APIClient do
       let(:response) { instance_double(Faraday::Response, success?: false, status: 404, body: 'Not Found') }
 
       before do
-        allow(client.instance_variable_get(:@connection)).to receive(:get).and_return(response)
+        allow(connection).to receive(:get).and_return(response)
       end
 
       it 'raises TRS::Errors::TeacherNotFound' do
@@ -56,7 +57,7 @@ RSpec.describe TRS::APIClient do
       let(:response) { instance_double(Faraday::Response, success?: false, status: 500, body: 'Internal Server Error') }
 
       before do
-        allow(client.instance_variable_get(:@connection)).to receive(:get).and_return(response)
+        allow(connection).to receive(:get).and_return(response)
       end
 
       it 'raises an error' do
@@ -65,22 +66,54 @@ RSpec.describe TRS::APIClient do
     end
   end
 
-  # FIXME: update this after we implement the API queries
   describe '#begin_induction!' do
-    context "calls logger" do
-      it "logs the call" do
-        expect(Rails.logger).to receive(:info).with("TRS API: begin_induction(#{trn}, 2021-09-01)")
-        client.begin_induction!(trn:, start_date: '2021-09-01')
-      end
+    let(:response) { instance_double(Faraday::Response, success?: true) }
+    let(:trn) { '0000123' }
+    let(:start_date) { '2024-01-01' }
+    let(:expected_payload) { { 'inductionStatus' => 'InProgress', 'startDate' => start_date }.to_json }
+
+    before do
+      allow(connection).to receive(:put).with("v3/persons/#{trn}/induction", expected_payload).and_return(response)
+    end
+
+    it "puts to the induction endpoint with the 'begin' parameters" do
+      client.begin_induction!(trn:, start_date:)
+
+      expect(connection).to have_received(:put).with("v3/persons/#{trn}/induction", expected_payload).once
     end
   end
 
-  describe '#complete_induction!' do
-    context "calls logger" do
-      it "logs the call" do
-        expect(Rails.logger).to receive(:info).with("TRS API: complete_induction(#{trn}, 2021-09-01, completed)")
-        client.complete_induction!(trn:, completion_date: '2021-09-01', status: 'completed')
-      end
+  describe '#pass_induction!' do
+    let(:response) { instance_double(Faraday::Response, success?: true) }
+    let(:trn) { '0000234' }
+    let(:completion_date) { '2024-02-02' }
+    let(:expected_payload) { { 'inductionStatus' => 'Pass', 'completionDate' => completion_date }.to_json }
+
+    before do
+      allow(connection).to receive(:put).with("v3/persons/#{trn}/induction", expected_payload).and_return(response)
+    end
+
+    it "puts to the induction endpoint with the 'pass' parameters" do
+      client.pass_induction!(trn:, completion_date:)
+
+      expect(connection).to have_received(:put).with("v3/persons/#{trn}/induction", expected_payload).once
+    end
+  end
+
+  describe '#fail_induction!' do
+    let(:response) { instance_double(Faraday::Response, success?: true) }
+    let(:trn) { '0000345' }
+    let(:completion_date) { '2024-03-03' }
+    let(:expected_payload) { { 'inductionStatus' => 'Fail', 'completionDate' => completion_date }.to_json }
+
+    before do
+      allow(connection).to receive(:put).with("v3/persons/#{trn}/induction", expected_payload).and_return(response)
+    end
+
+    it "puts to the induction endpoint with the 'fail' parameters" do
+      client.fail_induction!(trn:, completion_date:)
+
+      expect(connection).to have_received(:put).with("v3/persons/#{trn}/induction", expected_payload).once
     end
   end
 end
