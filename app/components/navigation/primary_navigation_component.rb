50c6e4faf3b8016659335b1d1b16bcbe2c61055e
Jack S <jack.scotti@education.gov.uk>
2024-10-22 14:04:45 +0100

    Move template logic to component #call method

--- app/components/navigation/primary_navigation_component.rb
+++ app/components/navigation/primary_navigation_component.rb
@@ -8,6 +8,20 @@ module Navigation
       @current_path = current_path
     end
 
+    def call
+      govuk_service_navigation(service_name: "Register early career teachers") do |service_navigation|
+        navigation_items.each do |item|
+          service_navigation.with_navigation_item(
+            text: item[:text],
+            href: item[:href],
+            current: current_page?(item[:href])
+          )
+        end
+      end
+    end
+
+  private
+
     def navigation_items
       [
         ab_nav_options,
@@ -15,8 +29,6 @@ module Navigation
         ({ text: "Sign out", href: sign_out_path } if is_authenticated),
       ].flatten.compact
     end
-
-  private
 
     def school_nav_options
       if current_path.start_with?("/school")
