f97a151a1839405453a12e0f333820fa2a113133
Peter Yates <peter.yates@graphia.co.uk>
2024-10-30 10:34:08 +0000

    Add unaccent to the list of allowed pg extensions
    We use this for standardising text for searching.

--- config/terraform/application/database.tf
+++ config/terraform/application/database.tf
@@ -12,7 +12,7 @@ module "postgres" {
   azure_enable_monitoring        = var.enable_monitoring
   azure_enable_backup_storage    = var.enable_postgres_backup_storage
   server_version                 = "16"
-  azure_extensions               = ["btree_gin", "citext", "plpgsql", "pg_trgm"]
+  azure_extensions               = ["btree_gin", "citext", "plpgsql", "pg_trgm", "unaccent"]
   azure_enable_high_availability = var.postgres_enable_high_availability
   azure_sku_name                 = var.postgres_flexible_server_sku
   azure_maintenance_window       = var.azure_maintenance_window
