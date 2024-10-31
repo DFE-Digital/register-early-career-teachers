90bfd16678ad2bf901550dc8e5a3e1bfc338c862
Peter Yates <peter.yates@graphia.co.uk>
2024-10-31 20:14:35 +0000

    Remove SQL execution from migration
    It's now run as a hook on db:prepare / db:schema:load so it's
    unnecessary here.

--- db/migrate/20241028105436_unaccent_teachers_search.rb
+++ db/migrate/20241028105436_unaccent_teachers_search.rb
@@ -1,31 +1,18 @@
 class UnaccentTeachersSearch < ActiveRecord::Migration[7.2]
   def down
+    # rubocop:disable Rails/BulkChangeTable
     remove_column :teachers, :search, :tsvector, type: :tsvector, as: "to_tsvector('unaccented', #{tsvector_columns})", stored: true
-
-    execute <<~SQL
-      drop text search configuration unaccented;
-    SQL
-
-    disable_extension 'unaccent'
-
     add_column :teachers, :search, :tsvector, type: :tsvector, as: "to_tsvector('english', #{tsvector_columns})", stored: true
+    add_index :teachers, :search, using: :gin
+    # rubocop:enable Rails/BulkChangeTable
   end
 
   def up
+    # rubocop:disable Rails/BulkChangeTable
     remove_column :teachers, :search, :tsvector, type: :tsvector, as: "to_tsvector('english', #{tsvector_columns})", stored: true
-
-    enable_extension 'unaccent'
-
-    execute <<~SQL
-      create text search configuration unaccented ( copy = simple );
-      alter text search configuration unaccented
-        alter mapping for hword, hword_part, word
-        with unaccent, simple;
-    SQL
-
     add_column :teachers, :search, :tsvector, type: :tsvector, as: "to_tsvector('unaccented', #{tsvector_columns})", stored: true
-
     add_index :teachers, :search, using: :gin
+    # rubocop:enable Rails/BulkChangeTable
   end
 
   def tsvector_columns
