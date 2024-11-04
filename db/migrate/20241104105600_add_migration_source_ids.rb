959a7b29adfec64340487c3ca0fb7ba5c070819a
Tony Headford <tony@binarycircus.com>
2024-11-04 11:34:20 +0000

    Bulk add columns in migration for rubocop

--- db/migrate/20241104105600_add_migration_source_ids.rb
+++ db/migrate/20241104105600_add_migration_source_ids.rb
@@ -2,16 +2,24 @@ class AddMigrationSourceIds < ActiveRecord::Migration[7.2]
   def change
     add_column :teachers, :legacy_id, :uuid
 
-    add_column :ect_at_school_periods, :legacy_start_id, :uuid 
-    add_column :ect_at_school_periods, :legacy_end_id, :uuid 
+    change_table :ect_at_school_periods, bulk: true do |t|
+      t.uuid :legacy_start_id
+      t.uuid :legacy_end_id
+    end
 
-    add_column :mentor_at_school_periods, :legacy_start_id, :uuid 
-    add_column :mentor_at_school_periods, :legacy_end_id, :uuid 
+    change_table :mentor_at_school_periods, bulk: true do |t|
+      t.uuid :legacy_start_id
+      t.uuid :legacy_end_id
+    end
 
-    add_column :mentorship_periods, :legacy_start_id, :uuid 
-    add_column :mentorship_periods, :legacy_end_id, :uuid 
+    change_table :mentorship_periods, bulk: true do |t|
+      t.uuid :legacy_start_id
+      t.uuid :legacy_end_id
+    end
 
-    add_column :training_periods, :legacy_start_id, :uuid 
-    add_column :training_periods, :legacy_end_id, :uuid 
+    change_table :training_periods, bulk: true do |t|
+      t.uuid :legacy_start_id
+      t.uuid :legacy_end_id
+    end
   end
 end
