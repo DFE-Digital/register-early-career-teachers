5ba6f7380530a53e04863c4501c585977780a406
Jack S <jack.scotti@education.gov.uk>
2024-11-08 20:03:29 +0000

    Render ruby in front matter

--- config/initializers/markdown_renderer.rb
+++ config/initializers/markdown_renderer.rb
@@ -2,13 +2,18 @@ module MarkdownRenderer
   def self.call(template, source)
     erb_handler = ActionView::Template.registered_template_handler(:erb)
     page_sections = source.match(/^\s*---(?<front_matter>.*?)---\s(?<markdown>.*)/m)
-    front_matter = page_sections[:front_matter] if page_sections
-    page_content = page_sections ? page_sections[:markdown] : source
-    compiled_source = erb_handler.call(template, page_content)
+
+    if page_sections
+      front_matter = erb_handler.call(template, page_sections[:front_matter])
+      page_content = erb_handler.call(template, page_sections[:markdown])
+    else
+      page_content = erb_handler.call(template, source)
+      front_matter = 'nil'
+    end
 
     <<~RUBY
-      page_data_from_front_matter('#{front_matter}')
-      MarkdownRenderer.render(begin; #{compiled_source}; end)
+      page_data_from_front_matter(begin; #{front_matter}; end)
+      MarkdownRenderer.render(begin; #{page_content}; end)
     RUBY
   end
 
--- spec/initializers/markdown_renderer_spec.rb
+++ spec/initializers/markdown_renderer_spec.rb
@@ -6,56 +6,85 @@ RSpec.describe MarkdownRenderer do
 
   before do
     allow(ActionView::Template).to receive(:registered_template_handler).with(:erb).and_return(erb_handler)
-    allow(erb_handler).to receive(:call).and_return('"processed markdown"')
   end
 
   describe ".call" do
     it "returns Ruby code string for markdown without frontmatter" do
       source = "# Hello World\n\nThis is a test"
+      allow(erb_handler).to receive(:call).with(template, source).and_return('"processed markdown"')
+
       rendered = MarkdownRenderer.call(template, source)
       expect(rendered).to eq(
         <<~RUBY
-          page_data_from_front_matter('')
+          page_data_from_front_matter(begin; nil; end)
           MarkdownRenderer.render(begin; "processed markdown"; end)
         RUBY
       )
     end
 
-    it "returns Ruby code string for markdown with frontmatter" do
+    it "processes ERB in both frontmatter and content" do
       source = <<~MARKDOWN
         ---
-        title: Test Title
+        title: <%= @page_title %>
+        author: <%= current_user.name %>
         ---
-        # Content
+        # <%= @heading %>
       MARKDOWN
+
+      # Expect ERB processing for both front matter and content
+      allow(erb_handler).to receive(:call)
+        .with(template, "\ntitle: <%= @page_title %>\nauthor: <%= current_user.name %>\n")
+        .and_return('"processed frontmatter"')
+
+      allow(erb_handler).to receive(:call)
+        .with(template, "# <%= @heading %>\n")
+        .and_return('"processed content"')
 
       rendered = MarkdownRenderer.call(template, source)
       expect(rendered).to eq(
         <<~RUBY
-          page_data_from_front_matter('
-          title: Test Title
-          ')
-          MarkdownRenderer.render(begin; "processed markdown"; end)
+          page_data_from_front_matter(begin; "processed frontmatter"; end)
+          MarkdownRenderer.render(begin; "processed content"; end)
         RUBY
       )
     end
 
-    it "returns Ruby code string for markdown with invalid frontmatter" do
+    it "handles markdown with invalid frontmatter while still processing ERB" do
       source = <<~MARKDOWN
         ---
-        invalid yaml content
+        invalid: <%= @yaml %> content
         ---
         # Content
       MARKDOWN
 
-      rendered = MarkdownRenderer.call(template, source)
+      allow(erb_handler).to receive(:call)
+        .with(template, "\ninvalid: <%= @yaml %> content\n")
+        .and_return('"processed invalid frontmatter"')
 
+      allow(erb_handler).to receive(:call)
+        .with(template, "# Content\n")
+        .and_return('"processed content"')
+
+      rendered = MarkdownRenderer.call(template, source)
       expect(rendered).to eq(
         <<~RUBY
-          page_data_from_front_matter('
-          invalid yaml content
-          ')
-          MarkdownRenderer.render(begin; "processed markdown"; end)
+          page_data_from_front_matter(begin; "processed invalid frontmatter"; end)
+          MarkdownRenderer.render(begin; "processed content"; end)
+        RUBY
+      )
+    end
+
+    it "processes ERB in content when no frontmatter is present" do
+      source = "# <%= @title %>\n\nThis is a <%= @type %> test"
+      allow(erb_handler).to receive(:call)
+        .with(template, source)
+        .and_return('"processed content with variables"')
+
+      rendered = MarkdownRenderer.call(template, source)
+      expect(rendered).to eq(
+        <<~RUBY
+          page_data_from_front_matter(begin; nil; end)
+          MarkdownRenderer.render(begin; "processed content with variables"; end)
         RUBY
       )
     end
