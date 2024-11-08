ea1cfd4ea07fa6f2be28b2b01bc16c16a4e9dc73
Peter Yates <peter.yates@graphia.co.uk>
2024-11-08 09:11:14 +0000

    Always ensure enable_personas is enabled for tests
    Co-authored-by: ltello <ltello8a@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module Ecf2
                        controller_specs: false,
                        helper_specs: false)
     end
+
+    config.enable_personas = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_PERSONAS', false))
+    config.enable_migration_testing = ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_MIGRATION_TESTING', false))
   end
 end
