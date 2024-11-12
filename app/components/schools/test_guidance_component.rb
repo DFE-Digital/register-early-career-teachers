e0c2f83950b7fc47ef71609b18699c941ee80d37
Peter Yates <peter.yates@graphia.co.uk>
2024-11-12 13:38:41 +0000

    Move example TRNs/dates of birth to slot
    This will allow us to use the guidance helper in a more flexible manner.

--- app/components/schools/test_guidance_component.rb
+++ app/components/schools/test_guidance_component.rb
@@ -1,26 +1,38 @@
 module Schools
   class TestGuidanceComponent < ViewComponent::Base
+    renders_one :trs_example_teacher_details, "TRSExampleTeacherDetails"
+
     def render?
       ActiveModel::Type::Boolean.new.cast(ENV.fetch('TEST_GUIDANCE', false))
     end
 
-    def head
-      ["TRN", "Date of birth"]
-    end
+    class TRSExampleTeacherDetails < ViewComponent::Base
+      def head
+        ["TRN", "Date of birth"]
+      end
 
-    def rows
-      [
-        %w[3002586 03/02/1977],
-        %w[3002585 02/01/1966],
-        %w[3002584 24/11/1955],
-        %w[3002583 24/09/1977],
-        %w[3002582 11/05/1980],
-        %w[3002580 04/03/1955],
-        %w[3002579 01/08/2001],
-        %w[3002578 10/09/1977],
-        %w[3002577 02/12/2000],
-        %w[3002576 04/03/1999],
-      ]
+      def rows
+        [
+          %w[3002586 03/02/1977],
+          %w[3002585 02/01/1966],
+          %w[3002584 24/11/1955],
+          %w[3002583 24/09/1977],
+          %w[3002582 11/05/1980],
+          %w[3002580 04/03/1955],
+          %w[3002579 01/08/2001],
+          %w[3002578 10/09/1977],
+          %w[3002577 02/12/2000],
+          %w[3002576 04/03/1999],
+        ]
+      end
+
+      def call
+        safe_join([
+          tag.h3('Information to review this journey', class: 'govuk-heading-m'),
+          tag.p('To successfully locate an ECT from the TRS API, use credentials from the table below:'),
+          govuk_table(head:, rows:)
+        ])
+      end
     end
   end
 end
--- spec/components/schools/test_guidance_component_spec.rb
+++ spec/components/schools/test_guidance_component_spec.rb
@@ -3,22 +3,34 @@ require "rails_helper"
 describe Schools::TestGuidanceComponent, type: :component do
   it "renders when TEST_GUIDANCE is true" do
     with_env_var("TEST_GUIDANCE", "true") do
-      render_inline(described_class.new)
-      expect(rendered_content).to include("Information to review this journey")
+      render_inline(described_class.new) { "some content" }
+      expect(rendered_content).to include("some content")
     end
   end
 
   it "does not render when TEST_GUIDANCE is false" do
     with_env_var("TEST_GUIDANCE", "false") do
       render_inline(described_class.new)
-      expect(rendered_content).not_to include("Information to review this journey")
+      expect(rendered_content).to be_blank
     end
   end
 
   it "does not render when TEST_GUIDANCE is missing" do
     with_env_var("TEST_GUIDANCE", nil) do
       render_inline(described_class.new)
-      expect(rendered_content).not_to include("Information to review this journey")
+      expect(rendered_content).to be_blank
+    end
+  end
+
+  describe 'TRS example details' do
+    it 'contains a table with TRNs and dates of birth' do
+      with_env_var("TEST_GUIDANCE", "true") do
+        render_inline(described_class.new) do |component|
+          component.with_trs_example_teacher_details
+        end
+
+        expect(rendered_content).to include('Information to review this journey')
+      end
     end
   end
 
