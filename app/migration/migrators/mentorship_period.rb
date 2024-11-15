3231c12abab858de5376f17543328e20edd47c8c
Tony Headford <tony@binarycircus.com>
2024-11-15 10:52:52 +0000

    wip adding migrator

---
+++ app/migration/migrators/mentorship_period.rb
@@ -0,0 +1,35 @@
+module Migrators
+  class MentorshipPeriod < Migrators::Base
+    def self.record_count
+      ects.count
+    end
+
+    def self.model
+      :mentorship_period
+    end
+
+    def self.ects
+      ::Teacher.where.associated(:ect_at_school_periods).distinct
+    end
+
+    def self.dependencies
+      %i[teacher]
+    end
+
+    def self.reset!
+      if Rails.application.config.enable_migration_testing
+        ::MentorshipPeriod.connection.execute("TRUNCATE #{::MentorshipPeriod.table_name} RESTART IDENTITY CASCADE")
+      end
+    end
+
+    def migrate!
+      migrate(self.class.ects) do |teacher|
+        migrate_mentorships!(teacher:)
+      end
+    end
+
+    def migrate_mentorships!(teacher:)
+    end
+
+  end
+end
