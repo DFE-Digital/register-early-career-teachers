0ffc5448c0266e393d461d21c006848e934fe748
Toby Retallick <toby.retallick@gmail.com>
2024-11-18 16:19:42 +0000

    refactor: team policy is no locales - so these have been removed and messages inlined

--- app/validators/date_of_birth_validator.rb
+++ app/validators/date_of_birth_validator.rb
@@ -5,8 +5,7 @@ class DateOfBirthValidator < ActiveModel::EachValidator
     date_of_birth = Schools::Validation::DateOfBirth.new(value)
 
     unless date_of_birth.valid?
-      message_scope = "errors.date_of_birth"
-      record.errors.add(attribute, I18n.t(date_of_birth.format_error, scope: message_scope))
+      record.errors.add(attribute, date_of_birth.format_error)
     end
   end
 end
