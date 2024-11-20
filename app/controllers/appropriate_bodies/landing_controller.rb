7bd31f52a19033e5c53d6f6f6d8c41220d8da4b4
Peter Yates <peter.yates@graphia.co.uk>
2024-11-20 11:25:41 +0000

    Fix the appropriate body landing page behaviour
    There was a bug here and the redirection wasn't covered by tests

--- app/controllers/appropriate_bodies/landing_controller.rb
+++ app/controllers/appropriate_bodies/landing_controller.rb
@@ -1,5 +1,10 @@
 module AppropriateBodies
   class LandingController < AppropriateBodiesController
+    skip_before_action :authorise,
+                       :authenticate,
+                       :set_appropriate_body,
+                       only: :show
+
     def show = nil
   end
 end
