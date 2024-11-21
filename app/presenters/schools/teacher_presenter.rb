25b6b8356fa5309a782613c0d3afb0bb16fa27ee
Lorenzo Tello <ltello8a@gmail.com>
2024-11-21 13:44:28 +0000

    flatten the data in the wizard store to easy access to it

--- app/presenters/schools/teacher_presenter.rb
+++ app/presenters/schools/teacher_presenter.rb
@@ -4,35 +4,32 @@ module Schools
   class TeacherPresenter
     private attr_reader :attributes
 
-    FIND_ECT_STEP = 'find_ect'
-    EMAIL_STEP = 'email_address'
-
     def initialize(attributes = {})
       @attributes = attributes
     end
 
     def full_name
-      first_name = attributes.dig(FIND_ECT_STEP, 'trs_first_name')
-      last_name = attributes.dig(FIND_ECT_STEP, 'trs_last_name')
+      first_name = attributes.dig('trs_teacher', 'trs_first_name')
+      last_name = attributes.dig('trs_teacher', 'trs_last_name')
 
       "#{first_name} #{last_name}".strip
     end
 
     def govuk_date_of_birth
-      dob = attributes.dig(FIND_ECT_STEP, 'date_of_birth')
+      dob = attributes.dig('trs_teacher', 'date_of_birth')
       dob&.to_date&.to_formatted_s(:govuk)
     end
 
     def trn
-      attributes.dig(FIND_ECT_STEP, 'trn')
+      attributes.dig('trn')
     end
 
     def email
-      attributes.dig(EMAIL_STEP, 'email')
+      attributes.dig('email')
     end
 
     def national_insurance_number
-      attributes.dig(FIND_ECT_STEP, 'trs_national_insurance_number')
+      attributes.dig('trs_teacher', 'trs_national_insurance_number')
     end
   end
 end
