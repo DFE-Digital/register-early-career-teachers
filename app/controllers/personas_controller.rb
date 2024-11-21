a17558d2b3309fb6f5be7e85b402d7d50b3d87ce
Peter Yates <peter.yates@graphia.co.uk>
2024-11-21 11:45:48 +0000

    Add persona images so they're a bit more personable

--- app/controllers/personas_controller.rb
+++ app/controllers/personas_controller.rb
@@ -1,5 +1,6 @@
 class PersonasController < ApplicationController
   skip_before_action :authenticate
+  layout 'full'
 
   def index
     @personas = Persona.all
--- app/helpers/personas_helper.rb
+++ app/helpers/personas_helper.rb
@@ -8,4 +8,27 @@ module PersonasHelper
     appropriate_body_name = appropriate_bodies.keys.find { |ab_name| Regexp.compile(ab_name) =~ name }
     appropriate_bodies[appropriate_body_name]
   end
+
+  def persona_name(persona)
+    split_persona_name(persona.name)[:name]
+  end
+
+  def persona_organisation(persona)
+    split_persona_name(persona.name)[:org]
+  end
+
+  def persona_image(name, height: '150px')
+    case
+    when name.start_with?('Velma') then image_tag('personas/velma.png', height:)
+    when name.start_with?('Fred') then image_tag('personas/fred.png', height:)
+    when name.start_with?('Daphne') then image_tag('personas/daphne.png', height:)
+    when name.start_with?('Norville') then image_tag('personas/shaggy.png', height:)
+    end
+  end
+
+private
+
+  def split_persona_name(name)
+    name.match(/(?<name>.*)\ \((?<org>.*)\)/)
+  end
 end
