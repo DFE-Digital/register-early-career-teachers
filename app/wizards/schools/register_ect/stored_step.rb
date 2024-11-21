ff8a82870b316f052561b4d581b2796a5f412922
Lorenzo Tello <ltello8a@gmail.com>
2024-11-21 13:54:12 +0000

    replace step handlers with a call to the #perform method on each step

--- app/wizards/schools/register_ect/stored_step.rb
+++ app/wizards/schools/register_ect/stored_step.rb
@@ -8,26 +8,34 @@ module Schools
       delegate :valid_step?, to: :wizard
       delegate :destroy_session, to: :store
 
+      def next_step; end
+
       def save!
         return false unless valid_step?
 
-        handler = StepStoreHandlerFactory.create!(
-          step_name: wizard.current_step_name,
-          wizard:,
-          store:,
-          key:
-        )
-        handler.perform
+        perform
       end
 
       def stored_attrs
         store
       end
 
+      def self.permitted_params
+        []
+      end
+
     private
 
       def key
         model_name.param_key.to_sym
+      end
+
+      def perform
+        step_params.each { |key, value| store.set(key, value) }
+      end
+
+      def step_params
+        wizard.step_params.to_h
       end
     end
   end
