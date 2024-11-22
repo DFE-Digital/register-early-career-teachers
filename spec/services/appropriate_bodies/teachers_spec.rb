78b0e3a0aacb941acaeb6a813deeecfaa384ecd9
Peter Yates <peter.yates@graphia.co.uk>
2024-11-22 11:16:36 +0000

    Add AppropriateBodies::Teachers class
    This will provide a sensible way for us to pull teachers associated with
    appropriate bodies. For now we only care about current ECTs but in the
    future we might want to extend this for past/completed ones too.

---
+++ spec/services/appropriate_bodies/teachers_spec.rb
@@ -0,0 +1,15 @@
+describe AppropriateBodies::Teachers do
+  let(:appropriate_body) { FactoryBot.create(:appropriate_body) }
+
+  describe "#current" do
+    subject { AppropriateBodies::Teachers.new(appropriate_body) }
+
+    it 'only returns records belonging to the current appropriate body' do
+      expect(subject.current.to_sql).to include(%(induction_periods"."appropriate_body_id" = #{appropriate_body.id}))
+    end
+
+    it 'only returns ongoing induction periods' do
+      expect(subject.current.to_sql).to include(%("induction_periods"."finished_on" IS NULL))
+    end
+  end
+end
