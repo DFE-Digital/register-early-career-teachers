9f620268e3f27c3260232b57c442015c288e38b8
Lorenzo Tello <ltello8a@gmail.com>
2024-11-26 12:53:15 +0000

    temporarily make the school landing page to pick a school with ECTs and no mentors

--- app/controllers/schools/home_controller.rb
+++ app/controllers/schools/home_controller.rb
@@ -1,18 +1,37 @@
 module Schools
   class HomeController < ApplicationController
+    layout "full"
+    before_action :set_school
+
     def index
-      teacher_service = Schools::Teacher.new(school[:id])
-      @school_name = school[:name]
+      teacher_service = Schools::Teacher.new(@school.id)
       @relationships = teacher_service.fetch_etcs_and_mentors
     end
 
   private
 
-    def school
+    def set_school
       # This is temporary. 'School' will be set once DfE signin hooked up
-      @school ||= School.joins(:gias_school)
-                        .select("schools.id, gias_schools.name")
-                        .first
+      # School in the session or first school with ects but no mentors or first school
+      @school = (school_from_session || first_school_with_no_mentors_but_ects || first_school).tap do |school|
+        session[:school_id] = school&.id
+      end
+    end
+
+    def school_from_session
+      School.joins(:gias_school).find_by_id(session[:school_id])
+    end
+
+    def first_school_with_no_mentors_but_ects
+      School.joins(:gias_school, :ect_at_school_periods)
+            .left_joins(:mentor_at_school_periods)
+            .where.not(ect_at_school_periods: { id: nil })
+            .where(mentor_at_school_periods: { id: nil })
+            .first
+    end
+
+    def first_school
+      School.joins(:gias_school).first
     end
   end
 end
