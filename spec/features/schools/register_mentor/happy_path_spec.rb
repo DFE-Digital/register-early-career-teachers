97d53af881738e686426e31736e381a851e862f7
Lorenzo Tello <ltello8a@gmail.com>
2024-11-27 10:51:12 +0000

    add find-mentor step to the register-mentor journey

--- spec/features/schools/register_mentor/happy_path_spec.rb
+++ spec/features/schools/register_mentor/happy_path_spec.rb
@@ -15,8 +15,12 @@ RSpec.describe 'Registering a mentor' do
     when_i_click_to_assign_a_mentor_to_the_ect
     then_i_am_in_the_requirements_page
 
-    when_i_click_the_back_link
-    then_i_should_be_taken_to_the_ects_page
+    when_i_click_continue
+    then_i_should_be_taken_to_the_find_mentor_page
+
+    when_i_submit_the_find_mentor_form(trn:, dob_day: '1', dob_month: '12', dob_year: '2000')
+    then_i_should_be_taken_to_the_review_mentor_details_page
+    and_i_should_see_the_mentor_details_in_the_review_page
   end
 
   def given_there_is_a_school_in_the_service
@@ -43,11 +47,30 @@ RSpec.describe 'Registering a mentor' do
     expect(page.url).to end_with("/school/register-mentor/what-you-will-need?ect_name=#{Rack::Utils.escape(@ect_name)}")
   end
 
-  def when_i_click_the_back_link
-    page.get_by_role('link', name: 'Back', exact: true).click
+  def when_i_click_continue
+    page.get_by_role('link', name: 'Continue').click
   end
 
-  def then_i_should_be_taken_to_the_ects_page
-    expect(page.url).to end_with('/schools/home/ects')
+  def then_i_should_be_taken_to_the_find_mentor_page
+    path = '/school/register-mentor/find-mentor'
+    expect(page.url).to end_with(path)
+  end
+
+  def when_i_submit_the_find_mentor_form(trn:, dob_day:, dob_month:, dob_year:)
+    page.get_by_label('trn').fill(trn)
+    page.get_by_label('day').fill(dob_day)
+    page.get_by_label('month').fill(dob_month)
+    page.get_by_label('year').fill(dob_year)
+    page.get_by_role('button', name: 'Continue').click
+  end
+
+  def then_i_should_be_taken_to_the_review_mentor_details_page
+    expect(page.url).to end_with('/school/register-mentor/review-mentor-details')
+  end
+
+  def and_i_should_see_the_mentor_details_in_the_review_page
+    expect(page.get_by_text(trn)).to be_visible
+    expect(page.get_by_text("Kirk Van Houten")).to be_visible
+    expect(page.get_by_text("1 December 2000")).to be_visible
   end
 end
