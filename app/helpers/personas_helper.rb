5d1f2c239c783d12ff4c24840b8bc45639a62dd1
Toby Retallick <toby.retallick@gmail.com>
2024-11-27 13:59:22 +0000

    feat: display the org type on the personas page (either a 'School' or 'Appropriate Body')

--- app/helpers/personas_helper.rb
+++ app/helpers/personas_helper.rb
@@ -17,6 +17,10 @@ module PersonasHelper
     split_persona_name(persona.name)[:org]
   end
 
+  def persona_user_type(persona)
+    split_persona_name(persona.name)[:org_type]
+  end
+
   def persona_image(name, height: '150px')
     case
     when name.start_with?('Velma')
@@ -24,15 +28,31 @@ module PersonasHelper
     when name.start_with?('Fred')
       image_tag('personas/fred.png', height:, alt: 'Fred from Scooby Doo. He is shrugging and wearing a white top with a red cravat.')
     when name.start_with?('Daphne')
-      image_tag('personas/daphne.png', height:, alt: 'Daphne from Scooby Doo looking quizical in a purple dress.')
+      image_tag('personas/daphne.png', height:, alt: 'Daphne from Scooby Doo looking quizzical in a purple dress.')
     when name.start_with?('Norville')
       image_tag('personas/shaggy.png', height:, alt: 'Shaggy from Scooby Doo, wearing a green shirt looking directly forwards.')
+    when name.start_with?('Bob')
+      image_tag('personas/bob.png', height:, alt: "Bob from Bob's Burgers, holding a hamburger")
+    when name.start_with?('Serena')
+      image_tag('personas/serena.png', height:, alt: 'Serena from Sailor Moon leaping in the air.')
     end
   end
 
 private
 
   def split_persona_name(name)
-    name.match(/(?<name>.*)\ \((?<org>.*)\)/)
+    match = name.match(/(?<name>.*)\ \((?<org>.*)\)/)
+
+    return nil unless match
+
+    org = match[:org]
+    org_type = case org
+               when /School$/i
+                 'School user'
+               when /Associate Body$/i
+                 'Appropriate body user'
+               end
+
+    { name: match[:name], org: org, org_type: org_type }
   end
 end
