1f0ea67e8b240c98f81d412b65fad8192f1c8525
Peter Yates <peter.yates@graphia.co.uk>
2024-11-29 13:44:50 +0000

    Fix documentation site's call to govuk_markdown

--- documentation/site/lib/filters/govuk_markdown.rb
+++ documentation/site/lib/filters/govuk_markdown.rb
@@ -2,5 +2,5 @@ require 'govuk_markdown'
 require 'debug'
 
 Nanoc::Filter.define(:govuk_markdown) do |content, _params|
-  GovukMarkdown.render(content, { headings_start_with: "l" }, { fenced_code_blocks: true })
+  GovukMarkdown.render(content, { headings_start_with: "l" })
 end
