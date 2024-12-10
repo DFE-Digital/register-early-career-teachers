1c3ef9260b96ff2fa3826537887a6a52ea7f6dab
Toby Retallick <toby.retallick@gmail.com>
2024-12-10 10:37:15 +0000

    fix: test subject

--- spec/services/schools/register_ect_spec.rb
+++ spec/services/schools/register_ect_spec.rb
@@ -1,4 +1,4 @@
-describe Schools::RegisterMentor do
+describe Schools::RegisterECT do
   let(:first_name) { "Dusty" }
   let(:last_name) { "Rhodes" }
   let(:trn) { "3002586" }
@@ -13,21 +13,21 @@ describe Schools::RegisterMentor do
                         started_on:)
   end
 
-  describe '#call' do
+  describe '#register_teacher!' do
     let(:teacher) { Teacher.first }
-    let(:mentor_at_school_period) { MentorAtSchoolPeriod.first }
+    let(:ect_at_school_period) { ECTAtSchoolPeriod.first }
 
     it 'creates a new Teacher record' do
-      expect { service.register_mentor! }.to change(Teacher, :count).from(0).to(1)
+      expect { service.register_teacher! }.to change(Teacher, :count).from(0).to(1)
       expect(teacher.first_name).to eq(first_name)
       expect(teacher.last_name).to eq(last_name)
       expect(teacher.trn).to eq(trn)
     end
 
     it 'creates an associated ECTATSchoolPeriod record' do
-      expect { service.register_mentor! }.to change(MentorAtSchoolPeriod, :count).from(0).to(1)
-      expect(mentor_at_school_period.teacher_id).to eq(teacher.id)
-      expect(mentor_at_school_period.started_on).to eq(started_on)
+      expect { service.register_teacher! }.to change(ECTAtSchoolPeriod, :count).from(0).to(1)
+      expect(ect_at_school_period.teacher_id).to eq(teacher.id)
+      expect(ect_at_school_period.started_on).to eq(started_on)
     end
 
     context "when no start date is provided" do
@@ -39,9 +39,9 @@ describe Schools::RegisterMentor do
       end
 
       it "current date is assigned" do
-        service.register_mentor!
+        service.register_teacher!
 
-        expect(mentor_at_school_period.started_on).to eq(Date.current)
+        expect(ect_at_school_period.started_on).to eq(Date.current)
       end
     end
   end
