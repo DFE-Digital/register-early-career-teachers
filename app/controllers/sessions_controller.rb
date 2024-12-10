957733353a4c586a805d775ef912e50caedf98b4
Peter Yates <peter.yates@graphia.co.uk>
2024-12-10 15:14:48 +0000

    Add logic to use appropriate login strategy
    DfE users will use OTP-style persona login and AB/School users will use
    DfE Sign-in style.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,12 +11,9 @@ class SessionsController < ApplicationController
     # NOTE: OTP authentication is handled in OTPSessionsController as it is not omniauth
     case user_info.provider
     when "developer"
-      session_manager.begin_persona_session!(
-        user_info.info.email,
-        name: user_info.info.name,
-        appropriate_body_id: params["appropriate_body_id"].presence,
-        school_urn: params["school_urn"].presence
-      )
+      dfe = ActiveModel::Type::Boolean.new.cast(params['dfe'])
+
+      dfe ? begin_otp_session(user_info) : begin_persona_session(user_info)
     when "dfe_sign_in"
       session_manager.begin_dfe_sign_in_session!(user_info)
     else
@@ -35,5 +32,20 @@ class SessionsController < ApplicationController
   def destroy
     session_manager.end_session!
     redirect_to root_path
+  end
+
+private
+
+  def begin_persona_session(user_info)
+    session_manager.begin_persona_session!(
+      user_info.info.email,
+      name: user_info.info.name,
+      appropriate_body_id: params["appropriate_body_id"].presence,
+      school_urn: params["school_urn"].presence
+    )
+  end
+
+  def begin_otp_session(user_info)
+    session_manager.begin_otp_session!(user_info.info.email)
   end
 end
